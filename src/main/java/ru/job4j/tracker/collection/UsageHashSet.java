52542f4678ba1164abd0de8722a86b2deac8f369
mason <mason2802@gmail.com>
2021-01-02 22:58:19 +0300

    0. Set, HashSet, Iterator. [#411603]

---
+++ src/main/java/ru/job4j/tracker/collection/UsageHashSet.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker.collection;
+import java.util.HashSet;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<>();
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
+        for (String mark : autos) {
+            System.out.println(mark);
+        }
+    }
+}
