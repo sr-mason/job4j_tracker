b3216645b17959c421c94066083d6606c1e47b3f
mason <mason2802@gmail.com>
2021-02-02 19:30:24 +0300

    2. Идентичные тексты [#411601]

---
+++ src/main/java/ru/job4j/tracker/collection/UsageMap.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker.collection;
+import java.util.HashMap;
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("ivanovIvan@mail.ru", "Ivanov Ivan Ivanovich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
